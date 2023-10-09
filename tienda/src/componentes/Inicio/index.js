bf5acf0e0b71a5a088663b76a6d6a1ee615650ab
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2023-10-08 21:11:41 -0600

    creacion de portada de inicio con responsive

--- tienda/src/componentes/Inicio/index.js
+++ tienda/src/componentes/Inicio/index.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Portada from "../../images/PantallaInicio.jpg";
+import { Link } from "react-router-dom";
+
 
 export const Inicio = () => {
   return (
-    <div>
-      <h1>Inicio</h1>
+    <div className="inicio">
+      <Link to="/">
+        <h1>Inicio</h1>
+        <div className="imagen-container">
+          <img src={Portada} alt="inicio" className="imagen-portada" />
+        </div>
+      </Link>
+      <Link to="/productos">
+        <h1>Productos</h1>
+      </Link>   
     </div>
-  )
-}
+  );
+};
 
